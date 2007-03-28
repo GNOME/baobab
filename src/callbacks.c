d5fa98a23f3bf3be5379690efbca8309643fb6af
Fabio Marzocca <marzocca@src.gnome.org>
2007-03-28 11:12:32 +0000

    bugzilla #421922
    svn path=/trunk/; revision=7470

--- src/callbacks.c
+++ src/callbacks.c
@@ -75,6 +75,23 @@ on_about_activate (GtkMenuItem *menuitem, gpointer user_data)
 		"Igalia (rings-chart widget) <www.igalia.com>",
 		NULL
 	};
+	
+	const gchar *license =
+    	"This program is free software; you can redistribute it and/or "
+    	"modify it under the terms of the GNU General Public License as "
+    	"published by the Free Software Foundation; either version 2 of "
+    	"the License, or (at your option) any later version.\n"
+    	"\n"
+    	"This program is distributed in the hope that it will be useful, "
+    	"but WITHOUT ANY WARRANTY; without even the implied warranty of "
+    	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU "
+    	"General Public License for more details.\n"
+    	"\n"
+    	"You should have received a copy of the GNU General Public License "
+    	"along with this program; if not, write to the Free Software "
+    	"Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA "
+    	"02111-1307, USA.\n";
+
 
 	static const gchar copyright[] = "Fabio Marzocca <thesaltydog@gmail.com> \xc2\xa9 2005-2006";
 
@@ -85,10 +102,12 @@ on_about_activate (GtkMenuItem *menuitem, gpointer user_data)
 			       "version", VERSION,
 			       "copyright", copyright,
 			       "logo-icon-name", "baobab",
-			       "license", "GPL",
+			       "license", license,
 			       "authors", authors, 
 			       "translator-credits",
-			       _("translator-credits"), NULL);
+			       _("translator-credits"), 
+			       "wrap-license", TRUE,
+			        NULL);
 }
 
 void
