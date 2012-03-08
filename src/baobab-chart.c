66ad37228e155fe6707064109eae2dd17cc1e969
Stefano Facchini <stefano.facchini@gmail.com>
2012-03-08 11:42:03 +0100

    baobab-chart: fix scrolling
    After recent GTK+ changes, GDK_SCROLL_MASK must be explicitly added.

--- src/baobab-chart.c
+++ src/baobab-chart.c
@@ -292,7 +292,7 @@ baobab_chart_realize (GtkWidget *widget)
   attributes.height = allocation.height;
   attributes.wclass = GDK_INPUT_OUTPUT;
   attributes.visual = gtk_widget_get_visual (widget);
-  attributes.event_mask = gtk_widget_get_events (widget) | GDK_EXPOSURE_MASK;
+  attributes.event_mask = gtk_widget_get_events (widget);
 
   attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
 
@@ -308,7 +308,8 @@ baobab_chart_realize (GtkWidget *widget)
   gtk_widget_add_events (widget,
                          GDK_EXPOSURE_MASK | GDK_BUTTON_PRESS_MASK |
                          GDK_BUTTON_RELEASE_MASK | GDK_POINTER_MOTION_MASK |
-                         GDK_POINTER_MOTION_HINT_MASK | GDK_LEAVE_NOTIFY_MASK);
+                         GDK_POINTER_MOTION_HINT_MASK | GDK_LEAVE_NOTIFY_MASK |
+                         GDK_SCROLL_MASK);
 }
 
 static void
