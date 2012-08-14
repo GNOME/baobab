5cf94147d4f298f86dca48973633357ce1294390
Paolo Borelli <pborelli@gnome.org>
2012-08-14 19:23:33 +0200

    Do not use gnome-common's autogen stuff

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,34 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-PKG_NAME="baobab"
+OLDDIR=`pwd`
+cd $srcdir
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README \
-  && test -f $srcdir/src/main.vala) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+    echo "*** No autoreconf found, please intall it ***"
     exit 1
-}
+fi
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common package"
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+    echo "*** No intltoolize found, please install the intltool package ***"
     exit 1
-}
+fi
 
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+GNOMEDOC=`which yelp-build`
+if test -z $GNOMEDOC; then
+    echo "*** The tools to build the documentation are not found,"
+    echo "    please intall the yelp-tools package ***"
+    exit 1
+fi
+
+autopoint --force || exit $?
+AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+
+cd $OLDDIR
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
